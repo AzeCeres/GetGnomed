14128de59a1b6373070027a5a573c2ce44ad91ee
AzeCeres <91120004+AzeCeres@users.noreply.github.com>
2023-03-23 10:50:27 +0100

    Initial commit

---
+++ Source/GetGnomed/GetGnomed.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class GetGnomed : ModuleRules
+{
+	public GetGnomed(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+	}
+}

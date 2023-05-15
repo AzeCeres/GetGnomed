df0def45491eb03d58f42ecb908f620731736982
AndreasStokvik <111863574+AndreasStokvik@users.noreply.github.com>
2023-05-15 19:26:23 +0200

    Enemy gets hit/gets killed sound
    also some small gain tweaks so your ears only die a bit when enemies die, also other unaltered audio files were added

--- Source/GetGnomed/TP_WeaponComponent.cpp
+++ Source/GetGnomed/TP_WeaponComponent.cpp
@@ -55,7 +55,7 @@ void UTP_WeaponComponent::Fire()
 	// Try and play the sound if specified
 	if (FireSound != nullptr)
 	{
-		UGameplayStatics::PlaySoundAtLocation(this, FireSound, Character->GetActorLocation());
+		//UGameplayStatics::PlaySoundAtLocation(this, FireSound, Character->GetActorLocation());
 	}
 	
 	// Try and play a firing animation if specified
